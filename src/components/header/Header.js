47cfa1c56afecc278f878603ecab0b9fdec33302
unknown <maruf.procode@gmail.com>
2022-11-24 08:08:48 +0600

    folder structure has been sated

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <div className='text-center'>
-            <h3>This is Header</h3>
+  return (
+    <div className="navbar shadow-md">
+      <div className="flex-1">
+        <Link className="btn btn-ghost normal-case text-xl">daisyUI</Link>
+      </div>
+      <div className="flex-none gap-2">
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered"
+          />
         </div>
-    );
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img src="https://placeimg.com/80/80/people" alt=""/>
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <Link className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </Link>
+            </li>
+            <li>
+              <Link>Settings</Link>
+            </li>
+            <li>
+              <Link>Logout</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/router/Routes.js
+++ src/router/Routes.js
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import ErrorPage from "../pages/errorPage/ErrorPage";
 import Home from "../pages/home/Home";
+import Login from "../pages/loginPage/Login";
+import SignUp from "../pages/SignUpPage/SignUp";
 
 const routes = createBrowserRouter([
     {
@@ -12,6 +14,18 @@ const routes = createBrowserRouter([
             {
                 path: '/',
                 element: <Home/>
+            },
+            {
+                path: '/home',
+                element: <Home/>
+            },
+            {
+                path: '/login',
+                element: <Login/>
+            },
+            {
+                path: '/signup',
+                element: <SignUp/>
             }
         ]
 
